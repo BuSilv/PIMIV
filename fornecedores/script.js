0bcdb9f0962857b468608dcd35beb3978caca155
Bruna <Brunah.v.04@gmail.com>
2024-10-09 22:32:38 -0300

    fix: clearcode js

--- fornecedores/script.js
+++ fornecedores/script.js
@@ -6,5 +6,35 @@ function show_proveedores(){
     window.location.href = "fornecedores.html";
 }
 
+function filterTable() {
+    const filter = document.getElementById("statusFilter").value;
+    const rows = document.querySelectorAll("#supplierTable tbody tr");
+
+    rows.forEach(row => {
+        const status = row.getAttribute("data-status");
+
+        if (filter === "all" || status === filter) {
+            row.style.display = "";
+        } else {
+            row.style.display = "none";
+        }
+    });
+}
+
+
+document.getElementById("searchBar").addEventListener("keyup", function () {
+    const searchText = this.value.toLowerCase();
+    const rows = document.querySelectorAll("#supplierTable tbody tr");
+
+    rows.forEach(row => {
+        const name = row.cells[0].innerText.toLowerCase();
+        if (name.includes(searchText)) {
+            row.style.display = "";
+        } else {
+            row.style.display = "none";
+        }
+    });
+});
+
 //import { show_orders } from '../pedidos/pedidos.js';
 //show_orders()
